d2814474a2ccac5f3d54fb828fa1e6021c9c7575
unknown <tecmatheusg@hotmail.com>
2023-05-19 10:04:05 -0300

    Injeção de dependência no construtor

---
+++ Alura.ByteBank.Infraestrutura.Testes/ClienteRepositoryTestes.cs
@@ -0,0 +1,34 @@
+﻿using Alura.ByteBank.Dados.Repositorio;
+using Alura.ByteBank.Dominio.Entidades;
+using Alura.ByteBank.Dominio.Interfaces.Repositorios;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public class ClienteRepositoryTestes
+    {
+        private readonly IClienteRepositorio _repositorio;
+
+        public ClienteRepositoryTestes()
+        {
+            var servico = new ServiceCollection();
+            servico.AddTransient<IClienteRepositorio, ClienteRepositorio>();
+            var provedor = servico.BuildServiceProvider();
+            _repositorio = provedor.GetService<IClienteRepositorio>();
+        }
+
+        [Fact]
+        public void TestaObterTodosClientes()
+        {
+            //Arrange            
+            //Act
+            List<Cliente> lista = _repositorio.ObterTodos();
+
+            //Assert
+            Assert.NotNull(lista);
+            Assert.Equal(3, lista.Count);            
+        }
+    }
+}
